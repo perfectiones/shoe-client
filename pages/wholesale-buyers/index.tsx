74af92ccf60ae1cc3e5e8d4cee0e31fee8153851
dmitriy oginov <dmitrij.oginov@mail.ru>
2023-09-28 01:32:36 +0300

    pushClient

---
+++ pages/wholesale-buyers/index.tsx
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+import { useCallback } from 'react'
+//import ContactsPage from '@/components/templates/ContactsPage/ContactsPage'
+import Layout from '@/components/layout/Layout'
+import Breadcrumbs from '@/components/modules/Breadcrumbs/Breadcrumbs'
+
+function WholesaleBuyers() {
+  const getDefaultTextGenerator = useCallback(() => 'Оптовым покупателям', [])
+  const getTextGenerator = useCallback((param: string) => ({}[param]), [])
+
+  return (
+    <>
+      <Head>
+        <title>Мордов ОЧка | Оптовым покупателям</title>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" type="image/svg" sizes="32x32" href="/img/logo.png" />
+      </Head>
+      <Layout>
+        <main>
+          <Breadcrumbs
+            getDefaultTextGenerator={getDefaultTextGenerator}
+            getTextGenerator={getTextGenerator}
+          />
+        </main>
+        <div className="overlay" />
+      </Layout>
+    </>
+  )
+}
+
+export default WholesaleBuyers
